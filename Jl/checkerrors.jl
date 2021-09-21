5f199d81eefd1e668353706c8754f647af5270c1
Jason Jia <jason.jia@chicagobooth.edu>
2021-09-21 14:53:08 -0500

    added checkerrors

---
+++ Jl/checkerrors.jl
@@ -0,0 +1,34 @@
+# Idea: If you find "The application encountered an internal error.", record the file name into a csv and save it.
+# To-do: What if xls is missing? What if pdf is missing? Cross-check file names...
+# To-do: download those next in line.
+
+using PDFIO
+using CSVFiles
+using DataFrames
+using CSV
+using Gumbo
+import Gumbo.text
+
+
+function errorxls(file)
+    filename=file[1:end-4]
+    list_html_raw=read(file,String)
+    if !isnothing(findfirst("The application encountered an internal error",list_html_raw))
+        error = "yes"
+    else
+        error = "no"
+    end
+    push!(dfErrorXls,[filename, error])
+    CSV.write("Errorxlsfiles.csv",dfErrorXls)
+end
+
+cd("C:/Users/jasonjia/Dropbox/ConferenceCall/Output/List3")
+files=readdir()
+dfErrorXls=DataFrame(filename=String[],error=String[])
+
+for file in files
+    if file[end-2:end]=="xls"
+        errorxls(file)
+    end
+end
+
