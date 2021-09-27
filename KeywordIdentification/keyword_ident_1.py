2ab844aae36c88acd7e794ff8bc14683662c3c57
Jason Jia <jason.jia@chicagobooth.edu>
2021-09-27 07:08:37 -0500

    keyword_ident_1 file path changes

--- KeywordIdentification/keyword_ident_1.py
+++ KeywordIdentification/keyword_ident_1.py
@@ -10,12 +10,13 @@ import sys
 import re
 
 
-csv_dir = "C:\\Users\\jasonjia\\\Dropbox\\ConferenceCall\\Output" # initially was sys.argv[1]
-target_dir = "C:\\Users\\jasonjia\\\Dropbox\\ConferenceCall\\Output" # initially was sys.argv[2]
+csv_dir = "C:/Users/jasonjia/Dropbox/ConferenceCall/Output/ConferenceCall/Csv" # initially was sys.argv[1]
+target_dir = "C:/Users/jasonjia/Dropbox/ConferenceCall/Output/KeywordIdentification" # initially was sys.argv[2]
 
 #%%
 #####　add any keywords in the text file ####
-with open("keyterms.txt", "r", encoding="utf-8", errors="ignore") as f1:
+with open("C:/Users/jasonjia/Dropbox/ConferenceCall/Output/KeywordIdentification/keyterms.txt", "r", encoding="utf-8", errors="ignore") as f1:
+
     key_set = set(f1.read().splitlines())
 
 keyw_list = list(key_set) 
@@ -139,8 +140,8 @@ total_data = pd.DataFrame({"Date":[], "Report":[], "Keywords":[], "Cri1":[], "Fi
 
 ###"Cri2":[], "Cri3":[],"Cri4":[], "File":[]})
 for each_csv in csv_list:
-    if "CC_List" in each_csv:
-        continue
+    if "CC_List" in each_csv: # don't run code if the document is CC_List. Run if it's not - i.e. if it's the processed csv file actually containing the call.
+        continue #to check for next file in csv_list
     file_csv = csv_dir + "/" + each_csv
     print(file_csv)
     data_frame = pd.read_csv(file_csv)
