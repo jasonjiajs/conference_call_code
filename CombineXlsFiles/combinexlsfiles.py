96c1df163e88586b7ad0aec9807d3093498b7030
Jason Jia <jason.jia@chicagobooth.edu>
2021-10-12 10:49:49 -0500

    added combinedxlsfiles

---
+++ CombineXlsFiles/combinexlsfiles.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from pathlib import Path
+
+# Overall structure:
+# Go into a folder.
+# Combine all the "xls" files.
+# Save into an actual excel file, in .xlsx format.
+
+inputfolder = Path(r"C:\Users\jasonjia\Dropbox\ConferenceCall\Misc\Why are Dates and Titles Missing\Filezilla")
+outputfolder = Path(r"C:\Users\jasonjia\Dropbox\ConferenceCall\Misc\Why are Dates and Titles Missing")
+outputfile = "xlsfiles_combined.xlsx"
+outputpath = Path(outputfolder / outputfile)
+
+table = pd.DataFrame()
+
+for file in inputfolder.iterdir(): 
+    if 'xls' in file.name:
+        print(file)
+        chunktable = pd.read_html(file)[0]
+        # chunktable = chunktable[['Date','Report #']]
+        table = table.append(chunktable)    
+
+writer = pd.ExcelWriter(outputpath)
+table.to_excel(writer, 'xlsfiles_combined')
+writer.save()
+
+
