458631698900f97776334232c1558d3d20884562
Jason Jia <jason.jia@chicagobooth.edu>
2021-12-07 13:15:25 -0600

    argparsed convert_computstat.py

--- 03_firm_identification/convert_compustat.py
+++ 03_firm_identification/convert_compustat.py
@@ -1,15 +1,20 @@
 from sas7bdat import SAS7BDAT
 from pathlib import Path
 import pandas as pd
+import argparse
 
-inputfolder = Path(r'C:\Users\jasonjia\Dropbox\Projects\ConferenceCall\Output\FirmIdentification')
-outputfolder = inputfolder
-outputsuffix = ''
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert Compustat .sas7bdat files to .csv files.')
+    parser.add_argument('inputfolder', help="input folder containing the .sas7bdat files.", type=str)
+    parser.add_argument('outputfolder', help="output folder that will contain the .csv files", type=str)
+    args = parser.parse_args()
+    inputfolder = Path(args.inputfolder)
+    outputfolder = Path(args.outputfolder)
 
 for file in inputfolder.iterdir():
     filestem = file.stem
     if file.suffix == '.sas7bdat':
-        outputfile = filestem + outputsuffix + '.csv'
+        outputfile = filestem + '.csv'
         outputpath = Path(outputfolder / outputfile)
         if outputpath.exists() == False:
             inputpath = Path(inputfolder / file)
