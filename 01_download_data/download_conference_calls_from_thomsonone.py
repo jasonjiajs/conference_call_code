04e3ee74a401aaf37c6e842fae8020229c83476c
Jason Jia <jason.jia@chicagobooth.edu>
2022-06-18 12:17:47 -0500

    updated to work for a good amount of time

--- 01_download_data/download_conference_calls_from_thomsonone.py
+++ 01_download_data/download_conference_calls_from_thomsonone.py
@@ -12,15 +12,16 @@ from tkinter import *
 
 # ------ Hardcode coordinates of different points on the screen ------
 coords = {
-'address_bar': (529, 35), 'screening_and_analysis': (665, 67), 'research': (306, 93), 'contributor': (152, 605), 
-'refinitiv_streetevents': (177, 641), 
+'address_bar': (529, 35), 'screening_and_analysis': (665, 67), 'research': (306, 93), 
+'contributor': (152, 605), 'refinitiv_streetevents': (177, 641), 
 'start_date': (383, 356), 'clear_start_date': (427, 352), 
 'end_date': (519, 353), 'clear_end_date': (581, 353), 
 'number_of_conference_calls': (157, 765), 'next_page': (1855, 764), 
-'select_all_main': (29, 830), 'view_main': (57, 797),
+'select_all_main': (29, 830), 'view_main': (57, 797), 
 'select_all_toc': (558, 319), 'view_toc': (556, 769),
-'xls_icon_multiple_pages': (1684, 763), 'xls_icon_one_page': (1714, 765),
-'reselect_thomsonone_window': (1034, 510)
+'reselect_thomsonone_window': (1034, 510),
+'xls_icon_multiple_pages_1': (1684, 763), 'xls_icon_multiple_pages_2': (1673, 763), 'xls_icon_one_page': (1714, 765),
+'xls_save_more_options': (1317, 1016), 'xls_save_as': (1389, 994)
 }
 
 # ------ Read in command-line arguments ------
@@ -35,14 +36,16 @@ if __name__ == '__main__':
     parser.add_argument('end_day', help="end date of pull - day (e.g. 1)", type=int)
     parser.add_argument('output_pdf_folder', help="path of output folder containing pdf files", type=str)
     parser.add_argument('output_xls_folder', help="path of output folder containing xls files", type=str)
+    parser.add_argument('relogin', help="whether to do the first login", type=int)
     args = parser.parse_args()
     suffix = args.suffix
     output_pdf_folder = Path(args.output_pdf_folder)
     output_xls_folder = Path(args.output_xls_folder) 
+    relogin = bool(args.relogin)
 
-# Sample command: python download_conference_calls_from_thomsonone.py 20210101_20220617 2021 01 01 2022 06 17 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_pdf_20210101_20220617 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_xls_20210101_20220617
+# Sample command: python download_conference_calls_from_thomsonone.py 20210101_20220617 2021 01 01 2022 06 17 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_pdf_20210101_20220617 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_xls_20210101_20220617 0
 
-# Sample command: python download_conference_calls_from_thomsonone.py 20210101_20220617 2021 01 01 2022 06 17 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_pdf_20210101_20220617 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_xls_20210101_20220617
+# Sample command: python download_conference_calls_from_thomsonone.py 20210101_20220617 2021 01 01 2022 05 08 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_pdf_20210101_20220617 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\01_download_cc\01.1_xls_20210101_20220617 0
 
 # ------ Print screen size and command-line arguments ------
 # Get screen size
@@ -50,9 +53,8 @@ root = Tk()
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
   
-print("--- Screen size ---")
+print("\n------ Screen size ------")
 print("width x height: %d x %d (pixels)" %(monitor_width, monitor_height))
-print("")
 
 # Set initial and end date
 date_start = datetime.date(year=args.start_year, month=args.start_month, day=args.start_day)
@@ -60,13 +62,13 @@ date_end = datetime.date(year=args.end_year, month=args.end_month, day=args.end_
 date_start_current = date_end + timedelta(days = 1)
 
 # Print command-line arguments
-print("--- Command-line arguments ---")
+print("\n------ Command-line arguments ------")
 print("Data pull used:", suffix)
 print("Start date:", date_start)
 print("End date:", date_end)
 print("Output pdf folder:", output_pdf_folder)
 print("Output xls folder:", output_xls_folder)
-print("")
+print("Initial login:", relogin)
 
 # Open keyboard and mouse Controller
 KB_enter = pynput.keyboard.Controller()
@@ -157,7 +159,7 @@ def enter_query(date_start_current, date_end_current, date_start_current_str, da
     enter_string(date_end_current_str, 0.5)
     
     # Search for conference calls and wait for results to display
-    press_key(Key.enter, 0.5) 
+    press_key(Key.enter, 2) 
 
 def get_number_of_conference_calls():
     pyperclip.copy("")
@@ -178,24 +180,29 @@ def get_number_of_pages_of_conf_calls(number_of_conference_calls):
 # ------ Main loop ------
 ### Note: Before you run the code, open Internet Explorer.
 
-print("Main loop starting in 5 seconds!")
-time.sleep(5)
+print("\n------ Main loop ------")
+print("Main loop starting in 3 seconds!")
+time.sleep(3)
 restarted_process = False
-login()
+if relogin:
+    login()
+relogin = False
 
 while date_start <= date_start_current:
-    print("---")
+    print("------")
 
     # Login and query with a 4-day time-window
     if restarted_process:
-        print("Trying new time window")
-        login()
-        restarted_process = False
-    else:
         print("Retrying previous time window")
-        date_start_current, date_end_current, date_range_current, date_start_current_str, date_end_current_str = get_new_time_window(date_start_current)
+        if relogin:
+            login()
+        restarted_process = False
+        relogin = False
+    else:
+        print("Trying new time window")
+        date_start_current, date_end_current, date_start_current_str, date_end_current_str, date_range_current = get_new_time_window(date_start_current)
     
-    enter_query(date_start_current, date_end_current, date_range_current, date_start_current_str, date_end_current_str)
+    enter_query(date_start_current, date_end_current, date_start_current_str, date_end_current_str, date_range_current)
     
     # Get number of conference calls and number of pages of conference calls to scroll through
     try:
@@ -209,10 +216,12 @@ while date_start <= date_start_current:
     except ValueError: 
         print("Error occurred when trying to turn number of conference calls into int, restarting process")
         restarted_process = True
+        relogin = False
         continue
     
     # Loop over each page
     for current_page in range(number_of_pages_of_conf_calls):
+        print("---")
         print("Currently at page", current_page + 1, "/", number_of_pages_of_conf_calls)
 
         # Get filepaths
@@ -224,8 +233,8 @@ while date_start <= date_start_current:
         # If pdf and xls already exists, skip page
         if os.path.exists(output_pdf_filepath) and os.path.exists(output_xls_filepath):
             print("Pdf and xls of current page already exists, skipping page")
-            mouse_click(coords['next_page'], 5) 
-            print("Finished page", current_page, "/", number_of_pages_of_conf_calls)
+            mouse_click(coords['next_page'], 3) 
+            print("Finished page", current_page + 1, "/", number_of_pages_of_conf_calls)
             continue
         
         # Else, select all reports and download the missing pdf and/or xls
@@ -233,48 +242,50 @@ while date_start <= date_start_current:
 
         # Get the pdf
         if os.path.exists(output_pdf_filepath):
-            print("pdf already exists, skipping pdf:", output_pdf_filepath)
+            print("Pdf already exists, skipping pdf:", output_pdf_filepath)
+            print("-")
         else:
-            print("pdf does not exist, downloading pdf:", output_pdf_filepath)
+            print("Pdf does not exist, downloading pdf:", output_pdf_filepath)
 
             # View -> Select all -> View
             mouse_click(coords['view_main'], 3)
             mouse_click(coords['select_all_toc'], 1)
-            mouse_click(coords['view_toc'], 6)
+            mouse_click(coords['view_toc'], 10)
             expand_screen(2)
 
             # if no new file is downloaded, keep rerunning the process up to the stop limit
             attempt = 1
             while not(os.path.exists(output_pdf_filepath)):
                 # Stop limit
-                print('Saving xls: attempt', attempt)
+                print('Saving pdf: attempt', attempt)
                 if attempt > 5: 
-                    print('Reached 5 attempts, pausing process for 1 minute for manual fix')
+                    print('Pausing process for 1 minute for manual fix')
                     time.sleep(60)
                 if attempt > 10: 
                     print('Reached max number of attempts (10), restarting process')
                     restarted_process = True
+                    relogin = True
                     break
                 attempt = attempt + 1
                 
                 # Save 
-                save_pdf_hotkey()
+                save_pdf_hotkey(1)
                 # Type the file name
-                enter_string(str(output_pdf_filepath), 0.5)
+                enter_string(str(output_pdf_filepath), 1)
                 press_key(Key.enter, 10)
-                close_window(5)
- 
-            # Close the inner and outer windows
-            print('Closing the window')
-            close_window(1)
-            close_window(1)
-            mouse_click(coords['reselect_thomsonone_window'],2) 
+                
+            # Clean-up
+            print("Pdf saved!")
+            print("-")
+            close_window(2)
+            close_window(2)
+            mouse_click(coords['reselect_thomsonone_window'], 2) 
 
         # Get the xls
         if os.path.exists(output_xls_filepath):
-            print("xls already exists, not downloading xls:", output_xls_filepath)
+            print("Xls already exists, not downloading xls:", output_xls_filepath)
         else:
-            print("xls does not exist, downloading xls:", output_xls_filepath)
+            print("Xls does not exist, downloading xls:", output_xls_filepath)
 
             # If no new file is downloaded, keep rerunning the process up to the stop limit
             attempt = 1
@@ -282,26 +293,30 @@ while date_start <= date_start_current:
                 # Stop limit
                 print('Saving xls: attempt', attempt)
                 if attempt > 5: 
-                    print('Reached 5 attempts, pausing process for 1 minute for manual fix')
+                    print('Pausing process for 1 minute for manual fix')
                     time.sleep(60)
                 if attempt > 10: 
                     print('Reached max number of attempts (10), restarting process')
                     restarted_process = True
+                    relogin = True
                     break
                 attempt = attempt + 1
 
                 # Click on the excel icon
                 if number_of_pages_of_conf_calls > 1:
-                    mouse_click(coords['xls_icon_multiple_pages'], 1)
+                    mouse_click(coords['xls_icon_multiple_pages_1'], 2)
+                    mouse_click(coords['xls_icon_multiple_pages_2'], 2)
                 else:
-                    mouse_click(coords['xls_icon_one_page'], 1)
+                    mouse_click(coords['xls_icon_one_page'], 2)
 
                 # Save -> save as
-                mouse_click(coords['xls_save_more_options'], 0.5)
+                mouse_click(coords['xls_save_more_options'], 1)
                 mouse_click(coords['xls_save_as'], 1)
                 # Type the file name
                 enter_string(str(output_xls_filepath), 0.5)
-                press_key(Key.enter, 1)
+                press_key(Key.enter, 2)
+
+            print("Xls saved!")
 
         # De-select the reports
         mouse_click(coords['select_all_main'], 1)
