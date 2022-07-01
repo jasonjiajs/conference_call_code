935c64fdf230ee59017785f2401837466c2f4103
Jason Jia <jason.jia@chicagobooth.edu>
2022-07-01 06:21:12 -0500

    corrected codes: lower case, date sort, gzip_combined

--- 03_identify_paragraphs_containing_keywords/combine_paragraph_gzip_files.py
+++ 03_identify_paragraphs_containing_keywords/combine_paragraph_gzip_files.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import argparse
 
 # Example command:
-# python combine_paragraph_gzip_files.py C:\Users\jasonjia\Dropbox\Projects\conference_call\output\03_identify_paragraphs_containing_keywords C:\Users\jasonjia\Dropbox\Projects\conference_call\output\03_identify_paragraphs_containing_keywords\20210101-20220617_paragraphs_containing_keywords_combined.gzip
+# python combine_paragraph_gzip_files.py C:\Users\jasonjia\Dropbox\Projects\conference_call\output\03_identify_paragraphs_containing_keywords\03.1_paragraphs_containing_keywords_20210101-20220617 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\03_identify_paragraphs_containing_keywords\03.1_paragraphs_containing_keywords_20210101-20220617\20210101-20220617_paragraphs_containing_keywords_combined.gzip
 
 # Overall structure:
 # Go into a folder.
@@ -22,16 +22,18 @@ if __name__ == '__main__':
 
 # Read in ".xls" files (which are really .html files)
 table = pd.DataFrame()
+count = 0
 for file in inputfolder.iterdir(): 
-    if file.suffix == '.gzip':
-        print("Reading:", file)
+    if file.suffix == '.gzip' and not('combined' in str(file)):
+        print("Reading:", file.name)
         # Read .gzip file into a pandas df and append to larger table
         chunktable = pd.read_parquet(file)
         chunktable['filestem'] = file.stem
         table = pd.concat([table, chunktable])
+        count = count + 1
 
 # Save table into excel
-print("\nSaving .gzip files combined into:", outputfilepath)
+print("\nSaving", count, ".gzip files combined into:", outputfilepath)
 table.to_parquet(outputfilepath, compression = "gzip")
 print("Saved!")
 
