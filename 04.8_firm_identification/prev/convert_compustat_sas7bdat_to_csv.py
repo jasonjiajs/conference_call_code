f6d021568d61293a3fa009704fbd1e4e3bbf074c
Jason Jia <jason.jia@chicagobooth.edu>
2022-07-05 12:20:46 -0500

    renamed old files

--- 04.8_firm_identification/prev/convert_compustat_sas7bdat_to_csv.py
+++ 04.8_firm_identification/prev/convert_compustat_sas7bdat_to_csv.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import pandas as pd
 import argparse
 
+# Example command: python convert_compustat_sas7bdat_to_csv.py C:\Users\jasonjia\Dropbox\Projects\conference_call\output\04_match_firm_names_to_gvkeys\04.1_process_compustat_and_hassan_files\compustat_raw\20220705 C:\Users\jasonjia\Dropbox\Projects\conference_call\output\04_match_firm_names_to_gvkeys\04.1_process_compustat_and_hassan_files\compustat_processed\20220705
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert Compustat .sas7bdat files to .csv files.')
     parser.add_argument('inputfolder', help="input folder containing the .sas7bdat files.", type=str)
@@ -12,15 +14,14 @@ if __name__ == '__main__':
     outputfolder = Path(args.outputfolder)
 
 for file in inputfolder.iterdir():
-    filestem = file.stem
     if file.suffix == '.sas7bdat':
-        outputfile = filestem + '.csv'
-        outputpath = Path(outputfolder / outputfile)
-        if outputpath.exists() == False:
-            inputpath = Path(inputfolder / file)
-            with SAS7BDAT(str(inputpath)) as reader:
+        outputfile = file.stem + '.csv'
+        outputfilepath = Path(outputfolder / outputfile)
+        if outputfilepath.exists() == False:
+            inputfilepath = Path(inputfolder / file)
+            with SAS7BDAT(str(inputfilepath)) as reader:
                 df = reader.to_data_frame()
-            df.to_csv(outputpath, index = None)
+            df.to_csv(outputfilepath, index = False)
             print("Converted from .sas7bdat to .csv: {}".format(file.name))
         else:
             print(".csv file already exists, did not convert: {}".format(file.name))
